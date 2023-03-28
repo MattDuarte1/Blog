1a7f94d8e8d5ed3e6f0d80627c2f24a78cdf4ffe
Mateus <mateus_duarte20@outlook.com>
2023-03-28 08:12:47 -0300

    feat: add Heading component

---
+++ src/atoms/Heading/index.tsx
@@ -0,0 +1,49 @@
+import { forwardRef } from 'react';
+import * as Styled from './styles';
+
+export type HeadingProps = {
+  children: React.ReactNode;
+  size?:
+    | 'xxsmall'
+    | 'xsmall'
+    | 'small'
+    | 'medium'
+    | 'large'
+    | 'xlarge'
+    | 'xxlarge'
+    | 'huge'
+    | 'xhuge';
+  as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  color?:
+    | 'white'
+    | 'lightGrey1'
+    | 'lightGrey2'
+    | 'grey'
+    | 'darkGrey'
+    | 'black'
+    | 'blue';
+  uppercase?: boolean;
+};
+
+const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(
+  (
+    { children, size = 'huge', as = 'h1', color, uppercase = false, ...props },
+    ref,
+  ) => {
+    return (
+      <Styled.Container
+        size={size}
+        ref={ref}
+        as={as}
+        color={color}
+        uppercase={uppercase}
+        {...props}
+      >
+        {children}
+      </Styled.Container>
+    );
+  },
+);
+
+Heading.displayName = 'HeadingComponent';
+export default Heading;
