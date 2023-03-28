a50db862b481e320985b80f7bc0fe4c4d43c8782
Mateus <mateus_duarte20@outlook.com>
2023-03-28 08:09:40 -0300

    feat: added the app and passing the contexts and extending the interface of the styled component

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from '@/contexts/Theme/provider';
+import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
+  );
+}
+
+export default MyApp;
