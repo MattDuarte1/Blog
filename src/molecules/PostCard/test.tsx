1acb688cff9a2d5a2d2bb57e95c499f83c8ab359
Mateus <mateus_duarte20@outlook.com>
2023-04-05 08:06:08 -0300

    fix: test bug

--- src/molecules/PostCard/test.tsx
+++ src/molecules/PostCard/test.tsx
@@ -2,16 +2,15 @@ import { screen } from '@testing-library/react';
 import PostCard from '.';
 import { renderTheme } from '@/styles/render-theme';
 import { theme } from '@/styles/theme';
-import mock from '@/organisms/PostSection/mock';
+import mock from '@/organisms/Posts/mock';
 
 describe('<PostCard>', () => {
   it('Should render a PostCard component', () => {
     const { container } = renderTheme(
-      <PostCard {...mock} onClick={jest.fn()} />,
+      <PostCard {...mock[1]} onClick={jest.fn()} />,
     );
 
     const postCard = container.firstElementChild;
-
     expect(container.firstElementChild).toBeInTheDocument();
     expect(postCard).toHaveStyle({
       width: '300px',
@@ -24,14 +23,14 @@ describe('<PostCard>', () => {
       'text-align': 'start',
       cursor: 'pointer',
     });
-    expect(screen.getByRole('heading', { name: mock.title }));
+    expect(screen.getByRole('heading', { name: mock[1].title }));
     expect(postCard.querySelector('h4').innerHTML).toBe(
-      mock.authorCompact.name,
+      mock[1].authorCompact.name,
     );
   });
 
   it('Should match snapshot component', () => {
-    renderTheme(<PostCard {...mock} onClick={jest.fn()} />);
+    renderTheme(<PostCard {...mock[1]} onClick={jest.fn()} />);
 
     expect(screen.getByRole('article')).toMatchSnapshot();
   });
