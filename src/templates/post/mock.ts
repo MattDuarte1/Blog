a166e4c296b1602908c01e24e672f6b233ca3912
Mateus <mateus_duarte20@outlook.com>
2023-04-10 09:37:40 -0300

    test: add tests and mock

---
+++ src/templates/post/mock.ts
@@ -0,0 +1,116 @@
+export default {
+  metaContent: [
+    { content: 'VOCÊ CONHECE JAVASCRIPT ?', attributes: null, tag: 'title' },
+    {
+      content: null,
+      attributes: {
+        property: 'og:title',
+        content: 'VOCÊ CONHECE JAVASCRIPT ?',
+      },
+      tag: 'meta',
+    },
+    {
+      content: null,
+      attributes: {
+        name: 'twitter:title',
+        content: 'VOCÊ CONHECE JAVASCRIPT ?',
+      },
+      tag: 'meta',
+    },
+    {
+      content: null,
+      attributes: {
+        name: 'description',
+        content: 'JAVASCRIPT: O QUE É, PARA QUE SERVE E COMO FUNCIONA O JS?',
+      },
+      tag: 'meta',
+    },
+    {
+      content: null,
+      attributes: {
+        property: 'og:description',
+        content: 'JAVASCRIPT: O QUE É, PARA QUE SERVE E COMO FUNCIONA O JS?',
+      },
+      tag: 'meta',
+    },
+    {
+      content: null,
+      attributes: {
+        name: 'twitter:description',
+        content: 'JAVASCRIPT: O QUE É, PARA QUE SERVE E COMO FUNCIONA O JS?',
+      },
+      tag: 'meta',
+    },
+    {
+      content: null,
+      attributes: {
+        property: 'og:image',
+        content:
+          'https://www.datocms-assets.com/90479/1672435007-javascript.png?auto=format&fit=max&w=1200',
+      },
+      tag: 'meta',
+    },
+    {
+      content: null,
+      attributes: { property: 'og:image:alt', content: 'Imagem Javascript' },
+      tag: 'meta',
+    },
+    {
+      content: null,
+      attributes: {
+        name: 'twitter:image',
+        content:
+          'https://www.datocms-assets.com/90479/1672435007-javascript.png?auto=format&fit=max&w=1200',
+      },
+      tag: 'meta',
+    },
+    {
+      content: null,
+      attributes: { name: 'twitter:image:alt', content: 'Imagem Javascript' },
+      tag: 'meta',
+    },
+    {
+      content: null,
+      attributes: { property: 'og:locale', content: 'en' },
+      tag: 'meta',
+    },
+    {
+      content: null,
+      attributes: { property: 'og:type', content: 'article' },
+      tag: 'meta',
+    },
+    {
+      content: null,
+      attributes: {
+        property: 'article:modified_time',
+        content: '2023-03-09T14:55:25Z',
+      },
+      tag: 'meta',
+    },
+    {
+      content: null,
+      attributes: { name: 'twitter:card', content: 'summary' },
+      tag: 'meta',
+    },
+  ],
+  id: '92631209',
+  title: 'VOCÊ CONHECE JAVASCRIPT ?',
+  image: {
+    url: 'https://www.datocms-assets.com/90479/1674241447-javascript1.svg',
+    alt: null,
+  },
+  createdAt: '2023-02-03T06:02:28+00:00',
+  description: 'JAVASCRIPT: O QUE É, PARA QUE SERVE E COMO USAR ?',
+  authorCompact: {
+    name: 'Mateus Duarte',
+    createdAt: '2023-01-12T06:37:19+00:00',
+    image: {
+      url: 'https://www.datocms-assets.com/90479/1673505399-67812250_126640048653747_8066872119595701241_n.jpg',
+      alt: 'author image',
+      title: 'Avatar',
+    },
+  },
+  content:
+    '<p>Se voc&ecirc; deseja se tornar um desenvolvedor web para poder desenvolver p&aacute;ginas completas, com layouts elegantes e conte&uacute;do din&acirc;mico, voc&ecirc; precisa conhecer o JavaScript.</p>\n<p>O JavaScript &eacute; a terceira etapa para construirmos um site profissional. No post anterior conhecemos as duas primeiras etapas:<span>&nbsp;</span><a href="https://www.treinaweb.com.br/blog/como-comecar-com-html-e-css/" title="o HTML e o CSS">o HTML e o CSS</a>, e nesse artigo vamos conhecer sobre como deixar o conte&uacute;do de nossas p&aacute;ginas mais interativo utilizando o JavaScript.</p>\n<h2>O que &eacute; JavaScript?</h2>\n<p>Antes de come&ccedil;armos a abordar o que vem a ser o JavaScript, &eacute; muito importante ressaltar que JavaScript n&atilde;o tem nada a ver com Java.</p>\n<p>Quando essa linguagem foi lan&ccedil;ada, ela tinha o nome oficial de LiveScript, criada pela Netscape. Como nessa &eacute;poca o Java estava conquistando cada vez mais o mercado, a Netscape quis aproveitar a popularidade do rec&eacute;m-lan&ccedil;ado Java, atrav&eacute;s de um acordo com a Sun, mudando ent&atilde;o seu nome para JavaScript.</p>\n<p>Essa mudan&ccedil;a ainda causa confus&atilde;o dando a impress&atilde;o de que a linguagem foi baseada em Java, mas n&atilde;o&hellip; foi tudo uma estrat&eacute;gia de marketing. Agora sim, podemos conhecer o que realmente vem a ser o JavaScript.</p>\n<p>O JavaScript &eacute; uma linguagem de programa&ccedil;&atilde;o interpretada, sendo a principal linguagem cliente-servidor em navegadores web. Ela &eacute; respons&aacute;vel pelo dinamismo de uma p&aacute;gina, ou seja, promover a intera&ccedil;&atilde;o com o usu&aacute;rio, sendo executados do lado do cliente. &Eacute; uma linguagem script com orienta&ccedil;&atilde;o a objetos din&acirc;mica e com tipagem fraca.</p>\n<p>Existem tr&ecirc;s camadas b&aacute;sicas no desenvolvimento web: o HTML e o CSS (como vimos no post anterior) e por fim a terceira camada que &eacute; o JavaScript, que como vimos acima, fica com a parte de comportamento da p&aacute;gina.</p>\n<h2>Quem usa JavaScript?</h2>\n<p>V&aacute;rias empresas e sites conhecidos utilizam JavaScript na parte de front-end, desde aplica&ccedil;&otilde;es mais simples como as mais complexas. Temos como exemplo a Google, Youtube, Facebook, Ebay, Yahoo, Twitter, Linkedin, dentre muitos outros. Quase toda aplica&ccedil;&atilde;o Web tem a necessidade de usar JavaScript. E, hoje em dia, o JavaScript n&atilde;o se limita apenas &agrave; web, ele tem sua vertente server-side, mobile etc.</p>\n<h2>Sintaxe do JavaScript</h2>\n<p>A sintaxe do JavaScript &eacute; muito simples. Voc&ecirc; pode utilizar o JavaScript dentro do seu c&oacute;digo HTML. Para criarmos o nosso cl&aacute;ssico &ldquo;Hello World&rdquo;, &eacute; s&oacute; colocamos a escrita dentro de um &ldquo;alert&rdquo;. Com o alert nosso texto aparecer&aacute; dentro de uma janela de alerta.</p>\n<p><img src="https://www.datocms-assets.com/90479/1678373667-javascript1.png" width="338" height="169" /></p>\n<p><code class=" language-html"></code>Realizar uma estrutura de condi&ccedil;&atilde;o no JavaScript tamb&eacute;m &eacute; simples:</p>\n<p><img src="https://www.datocms-assets.com/90479/1678373672-javascript2.png" width="358" height="149" /></p>\n<p>No JavaScript voc&ecirc; tamb&eacute;m pode definir suas pr&oacute;prias fun&ccedil;&otilde;es. Vamos ent&atilde;o escrever uma fun&ccedil;&atilde;o que recebe dois n&uacute;meros e realiza a opera&ccedil;&atilde;o de multiplica&ccedil;&atilde;o:</p>\n<p><img src="https://www.datocms-assets.com/90479/1678373677-javascript3.png" width="323" height="179" /></p>\n<p>Mercado de trabalho</p>\n<p>Como pudemos ver o JavaScript &eacute; uma das etapas do desenvolvimento web. Com isso, &eacute; obrigat&oacute;rio um desenvolvedor conhecer essa linguagem. Existem v&aacute;rias bibliotecas para se trabalhar com JavaScript, por isso &eacute; importante ter conhecimento de algumas delas, pois &eacute; muito comum elas serem requisitos para o preenchimento de vagas JavaScript.</p>\n<p>Al&eacute;m da adequa&ccedil;&atilde;o ao mercado de trabalho, conhecer essas bibliotecas garante um aumento de produtividade e facilidade em tarefas comuns do dia a dia.</p>\n<p>Sendo assim, como todos os sites possuem algum tipo de intera&ccedil;&atilde;o utilizando o JavaScript, dificilmente voc&ecirc; ficar&aacute; fora do mercado.</p>\n<h2>Como aprender JavaScript?</h2>\n<p>Aqui no TreinaWeb temos toda uma trilha voltada para o JavaScript. Para voc&ecirc; come&ccedil;ar nessa linguagem, recomendamos o curso &ldquo;JavaScript B&aacute;sico&rdquo;.</p>\n<p>Com ele, voc&ecirc; aprender&aacute; desde os conceitos b&aacute;sicos, fazendo com que voc&ecirc; entenda toda a estrutura da linguagem.</p>\n<p>Aliando a teoria com a pr&aacute;tica, voc&ecirc; tamb&eacute;m ir&aacute; desenvolver programas que ir&atilde;o interagir com os elementos de uma p&aacute;gina web.</p>\n<h2>Conhe&ccedil;a nossa metodologia</h2>\n<p>Nosso ambiente de aprendizagem &eacute; 100% online. Nossa metodologia de ensino &eacute; baseada em v&iacute;deo aulas, apostilas, exerc&iacute;cios aula-a-aula e exemplos completos. Voc&ecirc; estuda no seu tempo e ao final pode baixar o seu certificado digital ou solicitar o certificado impresso.</p>\n<p>Na parte escrita, fornecemos um material totalmente atualizado e completo para que voc&ecirc; possa usufruir do conte&uacute;do, utilizando-se de textos e imagens. J&aacute; as v&iacute;deo-aulas s&atilde;o bem explicativas e pr&aacute;ticas, abordando todo o conte&uacute;do passo-a-passo, para que voc&ecirc; entenda tudo da melhor maneira poss&iacute;vel.</p>\n<p>Al&eacute;m disso, voc&ecirc; encontrar&aacute; exerc&iacute;cios para poder fixar, praticar e aplicar todo o conte&uacute;do aprendido. S&atilde;o exerc&iacute;cios que s&atilde;o propostos em cada aula do curso. Eles podem variar entre alguns formatos, como por exemplo quest&otilde;es de escolha &uacute;nica, quest&otilde;es de m&uacute;ltipla escolha e complemento de trechos de c&oacute;digo.</p>\n<p>Se ainda assim voc&ecirc; tiver alguma d&uacute;vida, voc&ecirc; poder&aacute; pedir ajuda aos nossos professores atrav&eacute;s de tickets de atendimento. Funciona como um canal direto entre voc&ecirc; e o professor, onde voc&ecirc; poder&aacute; realizar perguntas que ser&atilde;o respondidas muito brevemente!</p>\n<p>Al&eacute;m disso, ainda pelo sistema suporte, voc&ecirc; tamb&eacute;m poder&aacute; consultar todas as perguntas que j&aacute; foram respondidas a outros alunos! =)</p>\n<h2>Quais conhecimentos preciso ter para iniciar este curso?</h2>\n<p>O curso &eacute; recomendado para voc&ecirc; iniciar seus estudos na linguagem JavaScript, no entanto, &eacute; interessante saber as bases necess&aacute;rias para aprender qualquer linguagem de programa&ccedil;&atilde;o: &ldquo;L&oacute;gica de Programa&ccedil;&atilde;o&rdquo; e &ldquo;L&oacute;gica de Programa&ccedil;&atilde;o Orientada a Objetos&rdquo;.</p>\n<h2>Gostou do que viu?</h2>\n<p>Se entusiasmou com o JavaScript? Gostaria de conhec&ecirc;-lo com mais detalhes? Sim, voc&ecirc; pode dar uma espiadinha. Acesse o curso de JavaScript e veja algumas aulas de exemplo. :)</p>\n<p>Bons estudos!</p>',
+  category: [{ title: 'javascript' }],
+};
---
+++ src/templates/post/test.tsx
@@ -0,0 +1,50 @@
+import { renderTheme } from '../../styles/render-theme';
+import { useSession } from 'next-auth/react';
+import { PostPage } from '.';
+import MockData from './mock';
+import { act, screen } from '@testing-library/react';
+
+jest.mock('next-auth/react');
+const mockUserSession = useSession as jest.Mock;
+mockUserSession.mockReturnValue({
+  status: 'unauthenticated',
+  data: null,
+});
+
+describe('<HomeTemplate>', () => {
+  it('Should render a PostTemplate component', async () => {
+    let container: HTMLElement;
+    await act(async () => {
+      const result = renderTheme(
+        <PostPage {...MockData} views="28" dataFormat="06/04/2023" />,
+      );
+
+      container = result.container;
+    });
+    const { title, description } = MockData;
+    const viewBox = container.querySelector('div[data-views]');
+
+    const newMock = [title, description];
+    expect(container.firstElementChild).toBeInTheDocument();
+    newMock.forEach((prop) =>
+      expect(screen.getByText(prop)).toBeInTheDocument(),
+    );
+    expect(screen.getByText(description)).toHaveStyle({
+      color: '#fff',
+    });
+    expect(viewBox).toHaveAttribute('data-views', '28');
+  });
+
+  it('Should match snapshot PostTemplate', async () => {
+    let container: HTMLElement;
+    await act(async () => {
+      const result = renderTheme(
+        <PostPage {...MockData} views="28" dataFormat="06/04/2023" />,
+      );
+
+      container = result.container;
+    });
+
+    expect(container).toMatchSnapshot();
+  });
+});
