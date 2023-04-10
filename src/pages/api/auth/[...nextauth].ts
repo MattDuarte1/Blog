e2801141158daa1d7830949a775d679c66208139
Mateus <mateus_duarte20@outlook.com>
2023-04-10 12:52:37 -0300

    fix: remove nextauth/mongodb-adapter because deprected and reinstall storybook

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,7 @@
-import clientPromise from '@/lib/mongodb';
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
 export default NextAuth({
-  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
